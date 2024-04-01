fa760afc8e8fc9bc1a0eceb13172d6d7da6072d8
erys <12535744+erys@users.noreply.github.com>
2024-04-01 16:32:11 -0400

    fixiup cors

--- config/cors.rb
+++ config/cors.rb
@@ -3,7 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins %r{\Ahttps://.*\.jjwxc\.net\z}
-    resource 'api/original_chapter', headers: :any, methods: %i[get post]
-    resource 'corrupt_chapter', headers: :any, methods: %i[cur_bytes cur_chapter_id]
+    resource '/api/*', headers: :any, methods: %i[get post]
   end
 end

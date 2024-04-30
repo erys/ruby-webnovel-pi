526739122ccb777555bdbaf2a395c110a173f619
erys <12535744+erys@users.noreply.github.com>
2024-04-30 13:48:23 -0400

    actually use classes

--- app/models/corrupt_character_list.rb
+++ app/models/corrupt_character_list.rb
@@ -23,11 +23,13 @@ class CorruptCharacterList
     og_chapter = OriginalChapter.find(original_chapter_id)
     return unless og_chapter.font_file.attached?
 
+    Rails.logger.info("glyphs: #{glyphs}")
+
     ttf = JjwxcTtf.from_string(og_chapter.font_file.download)
     all_characters.each do |corrupt_char|
       corrupt_char.glyph_md5 = ttf.md5_glyph(corrupt_char.og_bytes)
-      Rails.logger.info("bytes: #{corrupt_char.og_bytes.codepoints[0]}, glyph: #{corrupt_char.glyph_md5}")
       corrupt_char.likely_replacement = glyphs[corrupt_char.glyph_md5]
+      Rails.logger.info(corrupt_char)
     end
   rescue StandardError => e
     Rails.logger.error(e)

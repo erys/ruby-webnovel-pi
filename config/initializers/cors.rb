6b0e68f1ccd81a573fba28929b112382c4b63151
erys <12535744+erys@users.noreply.github.com>
2025-05-20 20:51:16 -0400

    add patch to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins %r{\Ahttps://.*\.jjwxc\.net\z}
-    resource '/api/*', headers: :any, methods: %i[get post]
+    resource '/api/*', headers: :any, methods: %i[get post patch]
   end
 end
